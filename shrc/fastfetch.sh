9604413f97af2df99b6b11017c37615045be09fa
GatlenCulp <GatlenCulp@gmail.com>
2024-12-21 04:12:41 -0500

    Added duck fastfetch

--- shrc/fastfetch.sh
+++ shrc/fastfetch.sh
@@ -1,5 +1,4 @@
 FASTFETCH_CONFIG="$HOME/.config/fastfetch"
 
 fastfetch --colors-block-range-start 9 --colors-block-width 3\
-    --config ${FASTFETCH_CONFIG}/lierb-fastfetch/presets/groups.jsonc\
-    --logo ${FASTFETCH_CONFIG}/gatlen-angry-adjust.png
+    --config ${FASTFETCH_CONFIG}/lierb-fastfetch/presets/groups.jsonc
