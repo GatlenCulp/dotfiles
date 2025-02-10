4170cb254d81cec3749f4a8a255c8188d61c81a1
GatlenCulp <GatlenCulp@gmail.com>
2025-02-09 22:13:12 -0500

    feat: add Avante and Dracula plugins to LunarVim config

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -4,4 +4,65 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
--- lvim.colorscheme = "Dracula"
+lvim.plugins = {
+  "Mofiqul/dracula.nvim",
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    opts = {
+      provider = "openai",
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o",
+        timeout = 30000,
+        temperature = 0,
+        max_tokens = 4096,
+      },
+    },
+    build = "make", -- Use "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" for Windows
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "echasnovski/mini.pick",
+      "nvim-telescope/telescope.nvim",
+      "hrsh7th/nvim-cmp",
+      "ibhagwan/fzf-lua",
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua",
+      {
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+    {
+      "xiyaowong/transparent.nvim",
+    }
+  },
+--   {
+--     "miversen33/sunglasses.nvim",
+--     config = true
+--   }
+}
+
+lvim.colorscheme = "Dracula"
+
