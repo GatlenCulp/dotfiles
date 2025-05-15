52cc9a7ff4edf88b45b27853edf2a1f094c6077c
GatlenCulp <GatlenCulp@gmail.com>
2025-05-15 00:50:23 -0400

    feat: Moved my nix-darwin config into a single flake

--- dot_config/nix-darwin/README.md
+++ dot_config/nix-darwin/README.md
@@ -6,5 +6,15 @@ This will install nix-darwin if you don't already have it.
 nix run nix-darwin -- switch --flake ~/.config/nix-darwin
 ```
 
+Everything should happen or be imported into `flake.nix`
+
+<!-- - `darwin.nix` -- nix-darwin configuration (Mac App Store apps,  Homebrew, etc.) -->
+
 <!-- This will also build if not already existed.
 darwin-rebuild switch --flake ~/.config/nix-darwin#gatty-2 -->
+
+NixOS inspired setup:
+https://github.com/notusknot/dotfiles-nix
+
+And a nix-darwin setup:
+https://github.com/heywoodlh/nix-darwin-flake
--- dot_config/nix-darwin/flake.nix
+++ dot_config/nix-darwin/flake.nix
@@ -5,11 +5,16 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
     configuration = { pkgs, ... }: {
+      # Import home-manager module
+      imports = [ home-manager.darwinModules.home-manager ];
+      
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
@@ -19,28 +24,107 @@
           pkgs.devenv
         ];
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+      # Use a custom configuration.nix location
+      environment.darwinConfig = "$HOME/.config/nix-darwin";
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+      # Auto upgrade nix package and the daemon service
+      services.nix-daemon.enable = true;
+      nix = {
+        package = pkgs.nix;
+        settings = {
+          "extra-experimental-features" = [
+            "nix-command"
+            "flakes"
+          ];
+        };
+      };
 
-      # Set Git commit hash for darwin-version.
+      # Create /etc/zshrc that loads the nix-darwin environment
+      programs = {
+        gnupg.agent.enable = true;
+        zsh.enable = true;
+      };
+
+      # Set Git commit hash for darwin-version
       system.configurationRevision = self.rev or self.dirtyRev or null;
 
       # User running nix-darwin
       users.users.gat = {
         name = "gat";
-        home = "Users/gat";
+        home = "/Users/gat";
+      };
+      
+      # Home Manager configuration
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.gat = { pkgs, ... }: {
+          home.stateVersion = "24.11";
+          # Add your home-manager configuration here
+          
+          # Git configuration (moved from system level to home-manager)
+          programs.git = {
+            enable = true;
+            userEmail = "GatlenCulp@gmail.com";
+            userName = "GatlenCulp";
+          };
+        };
       };
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
+      # Used for backwards compatibility, please read the changelog before changing
       system.stateVersion = 5;
 
-      programs.zsh.enable = true;
+      # Font configuration - updated to use packages instead of fonts
+      fonts.packages = [
+        pkgs.monaspace
+      ];
 
-      # The platform the configuration will be used on.
+      # Homebrew configuration
+      # homebrew = {
+      #   enable = true;
+
+      #   casks = [
+      #     "1password"
+      #     "bartender"
+      #     "brave-browser"
+      #     "fantastical"
+      #     "firefox"
+      #     "karabiner-elements"
+      #     "obsidian"
+      #     "raycast"
+      #     "soundsource"
+      #     "wezterm"
+      #   ];
+
+        # masApps = {
+        #   "Drafts" = 1435957248;
+        #   "Reeder" = 1529448980;
+        #   "Things" = 904280696;
+        #   "Timery" = 1425368544;
+        # };
+      # };
+
+      # System defaults
+      system.defaults = {
+        dock = {
+          autohide = true;
+          orientation = "left";
+          show-process-indicators = false;
+          show-recents = false;
+          static-only = true;
+        };
+        finder = {
+          AppleShowAllExtensions = true;
+          ShowPathbar = true;
+          FXEnableExtensionChangeWarning = false;
+        };
+        NSGlobalDomain = {
+          AppleKeyboardUIMode = 3;
+          "com.apple.keyboard.fnState" = true;
+        };
+      };
+
+      # The platform the configuration will be used on
       nixpkgs.hostPlatform = "aarch64-darwin";
     };
   in
